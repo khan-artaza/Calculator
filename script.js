99f96a4ddaa7f9588a810a3ae0e1491fef3fd3d3
Artaza <artazakhan556@gmail.com>
2025-01-21 01:30:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("disp");
+let button = document.getElementsByTagName("button");
+
+for(let i=0; i< button.length; i++){
+    button[i].addEventListener("click", ()=>{
+        let buttonVal = button[i].innerHTML;
+
+        if(buttonVal === "AC"){
+            display.value = ""
+        }
+        else if(buttonVal === "DEL"){
+            display.value = display.value.slice(0, -1);
+        }
+        else if(buttonVal === "="){
+            try{
+                let expression = display.value
+                    .replace(/÷/g, "/")
+                    .replace(/X/g, "*")
+                    .replace(/%/g, "/100*")
+
+                    display.value = eval(expression)
+            }catch{
+                display.value = "Error!"
+            }
+        }
+        else{
+            display.value += buttonVal
+        }
+    })
+}
